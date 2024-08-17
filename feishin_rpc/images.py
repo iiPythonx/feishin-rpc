594af6a51b0cb14f1922d3c7b57a8ed088ed0f1f
Pyx <91509874+pyxfluff@users.noreply.github.com>
2024-08-17 13:19:21 -0400

    Add imgbb proxy as an option (#1)
    * update images
    
    * update configuration
    
    * patch main
    
    * some changes
    
    ---------
    
    Co-authored-by: iiPython <ben@iipython.dev>

--- feishin_rpc/images.py
+++ feishin_rpc/images.py
@@ -8,6 +8,7 @@ from .config import config_data
 
 # Initialization
 proxy_url = config_data["proxy_url"]
+imgbb_key = config_data["imgbb_key"]
 
 # Constructors
 def construct_freeimagehost(art_url: str) -> str:
@@ -15,11 +16,18 @@ def construct_freeimagehost(art_url: str) -> str:
         resp = requests.post(
             "https://freeimage.host/api/1/upload",
             files = {"source": source.content},
-
-            # Yes, this key is meant to be here.
             params = {"key": "6d207e02198a847aa98d0a2a901485a5", "action": "upload"}
         ).json()
         return resp["image"]["url"]
+    
+def construct_imgbb(art_url: str) -> str:
+    with requests.get(art_url, verify = False) as source:
+        resp = requests.post(
+            "https://api.imgbb.com/1/upload",
+            files = {"image": source.content},
+            params = {"key": imgbb_key}
+        ).json()
+        return resp["data"]["url"]
 
 def construct_imgproxy(art_url: str) -> str:
     if "&v=" in art_url:  # Catch Navidrome since the default links are too large
@@ -30,5 +38,6 @@ def construct_imgproxy(art_url: str) -> str:
 image_constructors = {
     "freeimagehost": construct_freeimagehost,
     "imgproxy": construct_imgproxy,
+    "imgbb": construct_imgbb,
     "ndip": lambda x: f"{proxy_url.rstrip('/')}/image/{x.split('?id=')[1].split('&')[0]}/{x.split('&size=')[1]}" 
 }
