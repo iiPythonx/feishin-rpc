df52b8e162c6b862513e8cbbac991c2f49f45a50
iiPython <ben@iipython.dev>
2024-08-23 20:37:59 -0500

    fix ndip and efficiency improvement

--- feishin_rpc/images.py
+++ feishin_rpc/images.py
@@ -11,9 +11,10 @@ proxy_url = config_data["proxy_url"]
 imgbb_key = config_data["imgbb_key"]
 
 # Constructors
+session = requests.Session()
 def construct_freeimagehost(art_url: str) -> str:
-    with requests.get(art_url, verify = False) as source:
-        resp = requests.post(
+    with session.get(art_url, verify = False) as source:
+        resp = session.post(
             "https://freeimage.host/api/1/upload",
             files = {"source": source.content},
             params = {"key": "6d207e02198a847aa98d0a2a901485a5", "action": "upload"}
@@ -21,8 +22,8 @@ def construct_freeimagehost(art_url: str) -> str:
         return resp["image"]["url"]
     
 def construct_imgbb(art_url: str) -> str:
-    with requests.get(art_url, verify = False) as source:
-        resp = requests.post(
+    with session.get(art_url, verify = False) as source:
+        resp = session.post(
             "https://api.imgbb.com/1/upload",
             files = {"image": source.content},
             params = {"key": imgbb_key}
@@ -39,5 +40,5 @@ image_constructors = {
     "freeimagehost": construct_freeimagehost,
     "imgproxy": construct_imgproxy,
     "imgbb": construct_imgbb,
-    "ndip": lambda x: f"{proxy_url.rstrip('/')}/image/{x.split('?id=')[1].split('&')[0]}/{x.split('&size=')[1]}" 
+    "ndip": lambda x: f"{proxy_url.rstrip('/')}/image/{x.split('?id=')[1].split('&')[0]}" 
 }
